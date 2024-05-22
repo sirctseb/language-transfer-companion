4c60598e5dc4148f66cff2457f56295d1ef0de21
Christopher Best <bestchris@gmail.com>
2024-05-22 18:43:21 -0400

    add sentence practice to lesson 2

--- language-transfer-companion/src/app/lessons/2/page.tsx
+++ language-transfer-companion/src/app/lessons/2/page.tsx
@@ -2,13 +2,18 @@ import VocabList from "../VocabList";
 import { useData } from "../data/data";
 
 export default function Home() {
-  const { englishVocab, greekVocab } = useData(2);
+  const { englishVocab, greekVocab, exercisePrompts } = useData(2);
   return (
     <>
       <VocabList
         title="Lesson Vocab"
         englishWords={englishVocab}
         greekWords={greekVocab}
+      />
+      <VocabList
+        title="Practice"
+        englishWords={exercisePrompts[0].prompts}
+        greekWords={exercisePrompts[1].prompts}
       />
     </>
   );
--- language-transfer-companion/src/app/lessons/types.ts
+++ language-transfer-companion/src/app/lessons/types.ts
@@ -2,16 +2,24 @@ type EnglishVocab = {
   language: "english";
   word: string;
 };
+
 type GreekVocab = {
   language: "greek";
   word: string;
   latinized: string;
 };
-type ExercisePrompt = {
-  task: "english-to-greek" | "greek-to-english";
+
+type EnglishToGreekExercisePrompt = {
+  task: "english-to-greek";
   prompts: string[];
 };
+
+type GreekToEnglishExercisePrompt = {
+  task: "greek-to-english";
+  prompts: string[];
+};
+
 export type Data = {
   vocabulary: [EnglishVocab, GreekVocab][];
-  exercisePrompts: ExercisePrompt[];
+  exercisePrompts: [EnglishToGreekExercisePrompt, GreekToEnglishExercisePrompt];
 };
